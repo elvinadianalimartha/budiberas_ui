418101fd7234ba4d52a9ff9f8b344d7a90b6c14f
Elvina Diana <elvinadianalimartha@gmail.com>
2022-07-20 02:06:30 +0700

    connect to hosted API

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,7 @@
-const String baseUrl = 'http://192.168.100.36:8000/api';
-const String urlPhoto = 'http://192.168.100.36:8000/storage/';
+// const String baseUrl = 'http://192.168.100.36:8000/api';
+// const String urlPhoto = 'http://192.168.100.36:8000/storage/';
+
+const String baseUrl = 'https://budiberas.pinulusuran.my.id/api';
+const String urlPhoto = 'https://budiberas.pinulusuran.my.id/storage/';
 
 const String mapUrl = 'https://nominatim.openstreetmap.org';
