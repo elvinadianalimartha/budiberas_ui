df66efb77b8653ea2e19253df6b156415fc45ce7
Elvina Diana <elvinadianalimartha@gmail.com>
2022-09-17 08:07:47 +0700

    shared pref user token

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,15 @@
 // const String baseUrl = 'http://192.168.100.36:8000/api';
 // const String urlPhoto = 'http://192.168.100.36:8000/storage/';
 
+import 'package:shared_preferences/shared_preferences.dart';
+
 const String baseUrl = 'https://budiberas.pinulusuran.my.id/api';
 const String urlPhoto = 'https://budiberas.pinulusuran.my.id/photoProduct/';
 
 const String mapUrl = 'https://nominatim.openstreetmap.org';
+
+Future<String> getTokenUser() async{
+  SharedPreferences loginData = await SharedPreferences.getInstance();
+  var tokenUser = loginData.getString('token');
+  return tokenUser.toString();
+}
